1c5838af882332cce9f9d72598eaca2519fb9e41
sanketvispute7 <sanket.vispute@sptr.co>
2024-12-31 17:06:34 +0530

    remove env

--- src/views/MarriageView.vue
+++ src/views/MarriageView.vue
@@ -125,7 +125,7 @@ export default {
           alert("Please complete the CAPTCHA");
           return;
         }
-        const response = await fetch(import.meta.env.VITE_BACKEND_URL, {
+        const response = await fetch("https://eventapi-hjb8.onrender.com/", {
           method: "POST",
           headers: {
             "Content-Type": "application/json",
